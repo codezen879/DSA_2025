5fc0cd9f294617c39a19bb815521110cc8df6692
codezen879 <coder080302@gmail.com>
2025-04-28 12:44:42 +0530

    sort 012

---
+++ oracle/array/sort_012.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+   int l=0,m=0,h=n-1;
+   while(m<=h)
+   {
+      if(arr[m]==0)
+      {
+         int t = arr[m];
+         arr[m] = arr[l];
+         arr[l] = t;
+         l++;m++;
+      }
+      else if(arr[m]==1)m++;
+      else
+      {
+         int t = arr[m];
+         arr[m] = arr[h];
+         arr[h] = t;
+         h--;
+
+      }
+   }
+}
